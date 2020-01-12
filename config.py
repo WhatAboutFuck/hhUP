36eae09278edc86335d512174c5257ba98a9f3d5
unknown <UliusSpb@gmail.com>
2020-01-12 15:29:18 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+login,password,link = "login","Password","https://rostov.hh.ru/applicant/resumes"
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 Firefox/14.0.1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language':'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
+    'Accept-Encoding':'gzip, deflate',
+    'Connection':'keep-alive',
+    'DNT':'1'
+    }
---
+++ main.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import config 
+
+
+
+def Login():
+    options = Options()
+    options.add_argument(("user-agent={}").format(config.headers))
+    options.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    driver.get(config.link)
+    driver.find_element_by_name("username").send_keys(config.login)
+    driver.find_element_by_name("password").send_keys(config.password)
+    driver.find_element_by_xpath((".//input[@value='Войти в личный кабинет']")).click()
+    driver.implicitly_wait(4)
+    button = driver.find_element_by_class_name("applicant-resumes-recommendations-button")
+    if button.get_attribute("innerHTML").split(';')[-1].split("<!-- -->")[-1].split("</a>")[0] == "подходящие вакансии":
+        print("wait")
+    else:
+        button.click()
+    x = (driver.find_element_by_class_name("applicant-resumes-update").get_attribute("innerHTML"))
+    driver.close()
+    return x
+
+def Time(s=Login())->str:
+    '''Returns time to make a task for task manager'''
+    return s.split(";")[-1].split("<")[0]
+
+Time()
